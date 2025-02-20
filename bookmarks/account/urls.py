095fe6f73910a212d30f7820967d4720406645d7
Kiraogi <golokron3@yandex.ru>
2025-02-20 21:09:26 +0300

    ver 0.01.08
    Add password reset URLs to account app

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -11,9 +11,15 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
-    # url-адрес для сброса пароля
+    # url-адрес для смены пароля
     path('password-change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
     path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+
+    # url-адрес для сброса пароля
+    path('password-rest/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_rest_done'),
+    path('password-reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password-reset/complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 
     path('', views.dashboard, name='dashboard'),
 ]
