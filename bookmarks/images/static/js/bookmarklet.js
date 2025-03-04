e5a12ac69f73438536db221b478ffc2e923dd33f
Kiraogi <golokron3@yandex.ru>
2025-03-04 21:44:32 +0300

    ver 0.01.33.01
    Add CSS and HTML loading to bookmarklet.js

--- bookmarks/images/static/js/bookmarklet.js
+++ bookmarks/images/static/js/bookmarklet.js
@@ -3,3 +3,21 @@ const styleUrl = siteUrl + 'static/css/bookmarklet.css';
 const minWidth = 250;
 const minHeight = 250;
 
+// Загрузка CSS
+var head = document.getElementsByTagName('head')[0];
+var link = document.createElement('link');
+link.rel = 'stylesheet';
+link.type = 'text/css';
+link.href = styleUrl + '?r=' + Math.floor(Math.random()*99999999999999999);
+head.appendChild(link);
+
+// Загрузка HTML
+var body = document.getElementsByTagName('body')[0];
+boxHtml = `
+<div id="bookmarklet">
+<a href="#" id="close">&times;</a>
+<h1>Select an image to bookmark:</h1>
+<div class="images"></div>
+</div>`;
+body.innerHTML += boxHtml;
+
