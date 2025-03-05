9039d44206ff46581d849e74ef03e2fc1d960067
Kiraogi <golokron3@yandex.ru>
2025-03-05 21:29:07 +0300

    ver 0.01.35.1
    Fix bookmarklet script and remove redundant whitespace

--- bookmarks/images/templates/bookmarklet_launcher.js
+++ bookmarks/images/templates/bookmarklet_launcher.js
@@ -1,9 +1,7 @@
 (function(){
     if(!window.bookmarklet) {
-        bookmarklet_js = document.body.appendChild(document.
-        createElement('script'));
-        bookmarklet_js.scr = '//127.0.0.1:8000/static/js/bookmarklet.js?r='+Math.
-        floor(Math.random()*99999999999999999);
+        bookmarklet_js = document.body.appendChild(document.createElement('script'));
+        bookmarklet_js.srс = '//127.0.0.1:8000/static/js/bookmarklet.js?r='+Math.floor(Math.random()*9999999999999999);
         window.bookmarklet = true;
     }
     else {
