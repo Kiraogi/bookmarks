2aff5738c15a2a71edaf17bf94795f1a350fb33d
Kiraogi <golokron3@yandex.ru>
2025-03-05 20:39:22 +0300

    ver 0.01.34
    Add bookmarklet launch function and image filtering

--- bookmarks/images/static/js/bookmarklet.js
+++ bookmarks/images/static/js/bookmarklet.js
@@ -21,3 +21,32 @@ boxHtml = `
 </div>`;
 body.innerHTML += boxHtml;
 
+function bookmarkletLaunch() {
+    bookmarklet = document.getElementById('bookmarklet');
+    var imagesFound = bookmarklet.querySelector('.images');
+
+    // Очистить найденные изображения
+    imagesFound.innerHTML = '';
+    // Показать букмарклет
+    bookmarklet.style.display = 'block';
+    // Событие закрытия
+    bookmarklet.querySelector('#close').addEventListener('click', function() {
+        bookmarklet.style.display = 'none'
+    });
+
+    // Найти изображение в DOM с минимальными размерами
+    images = document.querySelectorAll('img[src$=".jpg"], img[src$=".jpeg"], img[src$=".png"]');
+    images.forEach(image => {
+        if (image.naturalWidth >= minWidth 
+            && image.naturalHeight >= minHeight)
+            {
+                var imageFound = document.createElement('img');
+                imageFound.src = image.src;
+                imagesFound.append(imageFound);
+            }
+        })
+    }
+
+
+// Запустить букмарклет
+bookmarkletLaunch();
