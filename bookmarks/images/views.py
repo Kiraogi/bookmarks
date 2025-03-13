b0f72e7941fe0bbe4e019ab7bb65142614d517c5
Kiraogi <golokron3@yandex.ru>
2025-03-13 21:21:20 +0300

    ver 0.01.44
    Add image list view and update URLs

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.shortcuts import get_object_or_404
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.views.decorators.http import require_POST
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .forms import ImageCreateForm
 from .models import Image
@@ -50,3 +51,28 @@ def image_like(request):
         except:
             pass
     return JsonResponse({'status': 'error'})
+
+@login_required
+def image_list(request):
+    images = Image.objects.all()
+    paginator = Paginator(images, 8)
+    page = request.GET.get('page')
+    images_only = request.GET.get('images_only')
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        # Если страница не является целым числом,
+        # то доставит первую страницу
+        images = paginator.page(1)
+    except EmptyPage:
+        if images_only:
+            # Если AJAX-запрос и страница вне диапазона,
+            #  то вернуть пустую страницу
+            return HttpResponse('')
+        # Если страница вне диапазона,
+        # то вернуть последнюю страницу результатов
+        images = paginator.page(paginator.num_pages)
+    if images_only:
+        return render(request, 'images/image/list_images.html', {'section': 'images', 'images': images})
+    return render(request, 'images/image/list.html', {'section': 'images', 'images': images})
+
