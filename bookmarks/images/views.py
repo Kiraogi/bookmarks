c7af30b1c0b68b5547e63c0ed93a319d9e43b8ae
Kiraogi <golokron3@yandex.ru>
2025-03-25 20:31:03 +0300

    ver 0.01.61
    Add image ranking feature and update views

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -40,6 +40,8 @@ def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
     # Увеличить общие число просмотров изображения на 1
     total_views = r.incr(f'image:{image.id}:views')
+    # Увеличить рейтинг изображения на 1
+    r.zincrby('image_ranking', 1, image.id)
     return render(request, 'images/image/detail.html', {'section': 'images', 
                                                         'image': image, 
                                                         'total_views': total_views})
@@ -90,3 +92,14 @@ def image_list(request):
 r = redis.Redis(host=settings.REDIS_HOST,
                 port=settings.REDIS_PORT,
                 db=settings.REDIS_DB)
+
+@login_required
+def image_ranking(request):
+    # Получить словарь рейтинга изображений
+    image_ranking = r.zrange('image_ranking', 0, -1, desc=True)[:10]
+    image_ranking_ids = [int(id) for id in image_ranking]
+    # Получить наиболее просматриваемые изображения
+    most_viewed = list(Image.objects.filter(id__in=image_ranking_ids))
+    most_viewed.sort(key=lambda x: image_ranking_ids.index(x.id))
+    return render(request, 'images/image/ranking.html', {'section': 'images', 
+                                                         'most_viewed': most_viewed})
