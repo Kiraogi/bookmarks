c6bae7be49b4f7d6502c57ed7cc6c8de56870b8c
Kiraogi <golokron3@yandex.ru>
2025-03-25 20:09:18 +0300

    ver 0.01.60
    Add image view count and Redis integration

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -5,10 +5,13 @@ from django.shortcuts import get_object_or_404
 from django.http import JsonResponse, HttpResponse
 from django.views.decorators.http import require_POST
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.conf import settings
 
 from .forms import ImageCreateForm
 from .models import Image
 from actions.utils import create_action
+
+import redis
 
 @login_required
 def image_create(request):
@@ -35,7 +38,11 @@ def image_create(request):
 
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
-    return render(request, 'images/image/detail.html', {'section': 'images', 'image': image})
+    # Увеличить общие число просмотров изображения на 1
+    total_views = r.incr(f'image:{image.id}:views')
+    return render(request, 'images/image/detail.html', {'section': 'images', 
+                                                        'image': image, 
+                                                        'total_views': total_views})
 
 @login_required
 @require_POST
@@ -79,3 +86,7 @@ def image_list(request):
         return render(request, 'images/image/list_images.html', {'section': 'images', 'images': images})
     return render(request, 'images/image/list.html', {'section': 'images', 'images': images})
 
+# Соединение с redis
+r = redis.Redis(host=settings.REDIS_HOST,
+                port=settings.REDIS_PORT,
+                db=settings.REDIS_DB)
